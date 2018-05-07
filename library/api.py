662b2039cda01050ac9f2630cb589a059c41dece
0xRumple <kasseryasser@windowslive.com>
2018-05-07 13:22:06 +0300

    Add hierarchy api

--- library/api.py
+++ library/api.py
@@ -8,22 +8,36 @@ class LessonListAPIView(generics.ListAPIView):
     queryset = models.Lesson.objects.all()
     serializer_class = serializers.LessonSerializer
 
+    def get_queryset(self):
+        return self.queryset.filter(modules__slug=self.kwargs.get('module_slug'))
+
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = models.Lesson.objects.all()
     serializer_class = serializers.LessonSerializer
     lookup_field = 'slug'
 
+    def get_queryset(self):
+        return self.queryset.filter(modules__slug=self.kwargs.get('module_slug'),
+                                    slug=self.kwargs.get('slug'))
+
 
 class ModuleListAPIView(generics.ListAPIView):
     queryset = models.Module.objects.all()
     serializer_class = serializers.ModuleSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(workshops__slug=self.kwargs.get('workshop_slug'))
 
 
 class ModuleRetrieveAPIView(generics.RetrieveAPIView):
     queryset = models.Module.objects.all()
     serializer_class = serializers.ModuleSerializer
     lookup_field = 'slug'
+
+    def get_queryset(self):
+        return self.queryset.filter(workshops__slug=self.kwargs.get('workshop_slug'),
+                                    slug=self.kwargs.get('slug'))
 
 
 class WorkshopListAPIView(generics.ListAPIView):
@@ -35,9 +49,13 @@ class WorkshopListAPIView(generics.ListAPIView):
 
 
 class WorkshopRetrieveAPIView(generics.RetrieveAPIView):
-    queryset = models.Track.objects.all()
+    queryset = models.Workshop.objects.all()
     serializer_class = serializers.WorkshopSerializer
     lookup_field = 'slug'
+
+    def get_queryset(self):
+        return self.queryset.filter(tracks__slug=self.kwargs.get('track_slug'),
+                                    slug=self.kwargs.get('slug'))
 
 
 class TrackListAPIView(generics.ListAPIView):
--- library/urls.py
+++ library/urls.py
@@ -8,7 +8,7 @@ api_urls = [
          api.LessonListAPIView.as_view(),
          name='lesson_list_api'),
 
-    path('tracks/<slug:track_slug>/workshops/<slug:workshop_slug>/modules/<slug:module_slug>/lessons/<slug:lesson_slug>/',
+    path('tracks/<slug:track_slug>/workshops/<slug:workshop_slug>/modules/<slug:module_slug>/lessons/<slug:slug>/',
          api.LessonRetrieveAPIView.as_view(),
          name='lesson_retrieve_api'),
 
@@ -16,7 +16,7 @@ api_urls = [
          api.ModuleListAPIView.as_view(),
          name='module_liste_api'),
 
-    path('tracks/<slug:track_slug>/workshops/<slug:workshop_slug>/modules/<slug:module_slug>/',
+    path('tracks/<slug:track_slug>/workshops/<slug:workshop_slug>/modules/<slug:slug>/',
          api.ModuleRetrieveAPIView.as_view(),
          name='module_retrieve_api'),
 
@@ -24,7 +24,7 @@ api_urls = [
          api.WorkshopListAPIView.as_view(),
          name='workshops_list_api'),
 
-    path('tracks/<slug:track_slug>/workshops/<slug:workshop_slug>',
+    path('tracks/<slug:track_slug>/workshops/<slug:slug>',
          api.WorkshopRetrieveAPIView.as_view(),
          name='workshops_retrieve_api'),
 
