a2808a2e881c6608fd8e7fa0bc47d379647614c9
Louay Hamada <loauy.hamada@gmail.com>
2018-05-22 03:48:07 +0300

    Review About Component: Data and Styles

--- spa/src/components/about/about.js
+++ spa/src/components/about/about.js
@@ -8,7 +8,10 @@ export default {
    name: 'AboutComponent',
    components: {},
    data: () => ({
-      imageSrc: [],
+      sectionsImages: [],
+      testimonialsImages: [],
+      teamImages: [],
+      mentorsImages: [],
       quotationIcon: '',
       starState: {
         active: '',
@@ -22,23 +25,12 @@ export default {
         twitter: '',
         LH: ''
       },
-      teamImages: [],
-      mentorsImages: [],
-      // mentorsOptions: {
-      //   querySelector: '.mentorscarousel',
-      //   draggable: true,
-      //   duration: 500,
-      //   easing: 'ease-out',
-      //   perPage: 4,
-      //   startIndex: 0,
-      //   rtl: false
-      // },
       navigation: {
         left: '',
         right: ''
       },
       currentSlide: 0,
-      descriptions: [
+      sections: [
         {
           header: 'رؤيتنا في الأكاديمية',
           paragraph: 'بما أننا نؤمن أن أفضل طريقة لتعلم البرمجة تكون من خلال التطبيق وليس من خلال الدروس النظرية التي لا يتم ربطها بشكل عملي مع مشاريع حقيق، فقد قمنا بإيجاد هذه المنصة لمساعدتك على تحقيق حلمك بالتدرب على لغة المستقبل (البرمجة) بأساليب فعالة.',
@@ -46,7 +38,7 @@ export default {
           alt: 'image 1 '
         },
         {
-          header: 'إبني مهاراتك بذكاء',
+          header: 'ابنِ مهاراتك بذكاء',
           paragraph: 'من الأخطاء الشائعة التي قد ترتكبها أثناء تعلمك للبرمجة هو إعتمادك على إسلوب خاطئ كدراسة كل المفاهيم دون الإلمام بكيفية توظيفها في مشروع حقيقي، لهذا في أكاديميتنا نقوم بتوجيهك من خلال مسار محدد من الورش التطبيقية بحيث أنك تتعلم المفاهيم في الوقت المناسب عند حاجتك لها.',
           src: 'about/building_blocks',
           alt: 'image 2 '
@@ -61,13 +53,13 @@ export default {
       testimonials: {
         title: 'مجتمعنا رائع يضم مئات المبرمجين',
         description: 'بإنضمامك معنا لن تتعلم البرمجة فحسب بل ستلتحق بمجتمع يوفر بيئة خصبة لتبادل الخبرات و بناء روابط و علاقات مع عدد كبير من المبرمجين الأكفاء',
-         carousel: [{
-            img: 'saif.png',
-            name: 'منصور الجاسمي',
-            description: 'مصمم تطبيقات موبايل',
-            quote: 'venenatis dui ac pharetra. Nunc eu felis fermentum diam molestie dictum. Ut pretium laoreet tincidunt. Curabitur suscipit iaculis elit et lobortis.',
-            rating: 2
-         },
+        carousel: [{
+          img: 'saif.png',
+          name: 'منصور الجاسمي',
+          description: 'مصمم تطبيقات موبايل',
+          quote: 'venenatis dui ac pharetra. Nunc eu felis fermentum diam molestie dictum. Ut pretium laoreet tincidunt. Curabitur suscipit iaculis elit et lobortis.',
+          rating: 2
+        },
          {
             img: 'saif.png',
             name: 'منصور الجاسمي',
@@ -104,12 +96,12 @@ export default {
       team: {
         title: 'فريق العمل',
         description: 'فريق عملنا يعمل على مدار الساعة ليحرص على حصولكم على الفائدة المرجوة و على أفضل تجربة خلال رحلتكم التدريبية على منصة coretabs',
-        set: [
+        members: [
           {
             name: 'Louay Hamada',
             about: 'Full-stack Web Application Developer',
             src: 'contributors/LouayH.jpg',
-            socials: [
+            links: [
               {
                 name: 'LH',
                 src: 'http://louayhamada.com'
@@ -136,7 +128,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'contributors/alhakem.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -164,12 +156,12 @@ export default {
       mentors: {
         title: 'الموجهون',
         description: 'فريق التوجية في الأكاديمية يتألف من عدد من الأشخاص المتميزين الذي سيقوموا بمساندتك أثناء رحلتك التدريبية على منصة coretabs',
-        set: [
+        members: [
           {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -196,7 +188,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -222,7 +214,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -249,7 +241,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -276,7 +268,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -303,7 +295,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -330,7 +322,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -357,7 +349,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -384,7 +376,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -411,7 +403,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -438,7 +430,7 @@ export default {
             name: 'Mohammed AlHakem',
             about: 'Front-end web developer & UI/UX designer',
             src: 'mentors/one.jpg',
-            socials: [
+            links: [
               {
                 name: 'github',
                 src: 'ss'
@@ -471,22 +463,22 @@ export default {
       perPage: 1
    }),
    methods: {
-     showTestimonials(i, e) {
-       let children = e.currentTarget.parentElement.children
-       for (let i = 0; i < children.length; i++) {
-         children[i].classList.remove('active')
-       }
-       e.currentTarget.classList.add('active')
-       this.testimonialsSiema.goTo(i)
-     },
-     showMentors(i, e) {
-       let children = e.currentTarget.parentElement.children
-       for (let i = 0; i < children.length; i++) {
-         children[i].classList.remove('active')
-       }
-       e.currentTarget.classList.add('active')
-       mentorsSiema.goTo(i * this.perPage)
-     },
+      showTestimonials(i, e) {
+        let children = e.currentTarget.parentElement.children
+        for (let i = 0; i < children.length; i++) {
+          children[i].classList.remove('active')
+        }
+        e.currentTarget.classList.add('active')
+        this.testimonialsSiema.goTo(i)
+      },
+      showMentors(i, e) {
+        let children = e.currentTarget.parentElement.children
+        for (let i = 0; i < children.length; i++) {
+          children[i].classList.remove('active')
+        }
+        e.currentTarget.classList.add('active')
+        mentorsSiema.goTo(i * this.perPage)
+      },
       calc(i, j) {
         return i <= j ? this.starState.active : this.starState.inactive
       },
@@ -501,15 +493,15 @@ export default {
       },
       playMSiema() {
         carouselTransitionM = setInterval(() => {
-          if (mentorsSiema.currentSlide + mentorsSiema.perPage >= this.mentors.set.length) {
-            mentorsSiema.prev(this.mentors.set.length)
+          if (mentorsSiema.currentSlide + mentorsSiema.perPage >= this.mentors.members.length) {
+            mentorsSiema.prev(this.mentors.members.length)
           } else {
             mentorsSiema.next(mentorsSiema.perPage)
           }
         }, 5000)
       },
-      count() {
-        return Math.ceil(this.mentors.set.length / this.perPage)
+      mentorsSliderCount() {
+        return Math.ceil(this.mentors.members.length / this.perPage)
       },
       prev() {
         var index = mentorsSiema.currentSlide / mentorsSiema.perPage
@@ -542,170 +534,171 @@ export default {
         mentorsSiema.next(mentorsSiema.perPage)
       }
    },
-   created() {
-     for (let k of this.descriptions) {
-       this.$store.dispatch('getImgUrl', `images/${k.src}.svg`).then(img => {
-          this.imageSrc.push(img)
-       }).catch(error => {
-          throw new Error(error.message)
-       })
-     }
-
-      this.$store.dispatch('getImgUrl', 'images/quotation.svg').then(img => {
-         this.quotationIcon = img
+  created() {
+    for (let section of this.sections) {
+      this.$store.dispatch('getImgUrl', `images/${section.src}.svg`).then(img => {
+        this.sectionsImages.push(img)
       }).catch(error => {
-         throw new Error(error.message)
+        throw new Error(error.message)
       })
+    }
 
-      this.$store.dispatch('getImgUrl', 'images/star_active.svg').then(img => {
-         this.starState.active = img
+    for (let testimonial of this.testimonials.carousel) {
+      this.$store.dispatch('getImgUrl', `images/testimonials/${testimonial.img}`).then(img => {
+        this.testimonialsImages.push(img)
       }).catch(error => {
-         throw new Error(error.message)
+        throw new Error(error.message)
       })
-      this.$store.dispatch('getImgUrl', 'images/star_inactive.svg').then(img => {
-         this.starState.inactive = img
+    }
+
+    this.$store.dispatch('getImgUrl', 'images/quotation.svg').then(img => {
+      this.quotationIcon = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    this.$store.dispatch('getImgUrl', 'images/star_active.svg').then(img => {
+      this.starState.active = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    this.$store.dispatch('getImgUrl', 'images/star_inactive.svg').then(img => {
+      this.starState.inactive = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    for (let icon in this.icons) {
+      this.$store.dispatch('getImgUrl', `icons/social-icons/${icon !== 'LH' ? icon + '.svg' : icon + '.png'}`).then(img => {
+        this.icons[icon] = img
       }).catch(error => {
-         throw new Error(error.message)
+        throw new Error(error.message)
       })
+    }
 
-      var root = this
-      root.testimonials.carousel.forEach(function(testimonial) {
-         root.$store.dispatch('getImgUrl', 'images/testimonials/' + testimonial.img).then(img => {
-            testimonial.img = img
-         }).catch(error => {
-            throw new Error(error.message)
-         })
+    for (let member of this.team.members) {
+      this.$store.dispatch('getImgUrl', `images/${member.src}`).then(img => {
+        this.teamImages.push(img)
+      }).catch(error => {
+        throw new Error(error.message)
       })
+    }
 
-      for (let k in this.icons) {
-        this.$store.dispatch('getImgUrl', `icons/social-icons/${k !== 'LH' ? k + '.svg' : k + '.png'}`).then(img => {
-           this.icons[k] = img
-        }).catch(error => {
-           throw new Error(error.message)
-        })
+    for (let member of this.mentors.members) {
+      this.$store.dispatch('getImgUrl', `images/${member.src}`).then(img => {
+        this.mentorsImages.push(img)
+      }).catch(error => {
+        throw new Error(error.message)
+      })
+    }
+
+    this.$store.dispatch('getImgUrl', 'images/left.svg').then(img => {
+      this.navigation.left = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    this.$store.dispatch('getImgUrl', 'images/right.svg').then(img => {
+      this.navigation.right = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+  },
+  mounted() {
+    this.testimonialsSiema = new Siema({
+      selector: '.testimonials-carousel',
+      duration: 500,
+      easing: 'ease-out',
+      perPage: 1,
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: true,
+      rtl: false,
+      onInit: changeCurrentSlideT,
+      onChange: changeCurrentSlideT
+    })
+
+    this.playTSiema()
+
+    function changeCurrentSlideT() {
+      const slideT = document.querySelector('.t')
+      if (slideT) {
+        var children = slideT.children
+        for (var i = 0; i < children.length; i++) {
+          children[i].classList.remove('active')
+        }
+        children[this.currentSlide].classList.add('active')
       }
+    }
 
-      for (let k of this.team.set) {
-        this.$store.dispatch('getImgUrl', `images/${k.src}`).then(img => {
-           this.teamImages.push(img)
-        }).catch(error => {
-           throw new Error(error.message)
-        })
+    let childrenT = this.$refs.controlsT.children
+    for (let i = 0; i < childrenT.length; i++) {
+      childrenT[i].classList.remove('active')
+    }
+    childrenT[0].classList.add('active')
+
+    let root = this
+
+    mentorsSiema = new Siema({
+      selector: '.mentors-carousel',
+      duration: 1000,
+      easing: 'ease-out',
+      perPage: {
+        1150: 4,
+        900: 3,
+        650: 2,
+        520: 3,
+        0: 2
+      },
+      startIndex: 0,
+      draggable: true,
+      multipleDrag: true,
+      threshold: 20,
+      loop: false,
+      rtl: false,
+      onInit: changeCurrentSlideM,
+      onChange: changeCurrentSlideM
+    })
+
+    this.playMSiema()
+
+    function changeCurrentSlideM() {
+      var index = this.currentSlide / this.perPage
+      if (this.currentSlide % this.perPage === 0 || index + 1 === root.mentors.members.length / this.perPage) {
+        const slideM = document.querySelector('.m')
+        if (slideM) {
+          var children = slideM.children
+          for (var i = 0; i < children.length; i++) {
+            children[i].classList.remove('active')
+          }
+          index = Math.ceil(index)
+          children[index].classList.add('active')
+        }
       }
+    }
 
-      for (let k of this.mentors.set) {
-        this.$store.dispatch('getImgUrl', `images/${k.src}`).then(img => {
-           this.mentorsImages.push(img)
-        }).catch(error => {
-           throw new Error(error.message)
-        })
+    addEventListener('resize', function() {
+      root.perPage = mentorsSiema.perPage
+      const slideM = document.querySelector('.m')
+      if (slideM) {
+        var children = slideM.children
+        for (var i = 0; i < children.length; i++) {
+          children[i].classList.remove('active')
+        }
+        children[0].classList.add('active')
+        mentorsSiema.currentSlide = 0
       }
+    }, false)
 
-      this.$store.dispatch('getImgUrl', 'images/left.svg').then(img => {
-         this.navigation.left = img
-      }).catch(error => {
-         throw new Error(error.message)
-      })
-      this.$store.dispatch('getImgUrl', 'images/right.svg').then(img => {
-         this.navigation.right = img
-      }).catch(error => {
-         throw new Error(error.message)
-      })
-   },
-   mounted() {
-     this.testimonialsSiema = new Siema({
-        selector: '.testimonials-carousel',
-        duration: 500,
-        easing: 'ease-out',
-        perPage: 1,
-        startIndex: 0,
-        draggable: true,
-        multipleDrag: true,
-        threshold: 20,
-        loop: true,
-        rtl: false,
-        onInit: changeCurrentSlideT,
-        onChange: changeCurrentSlideT
-     })
+    let childrenM = this.$refs.controlsT.children
+    for (let i = 0; i < childrenM.length; i++) {
+      childrenM[i].classList.remove('active')
+    }
+    childrenM[0].classList.add('active')
 
-     this.playTSiema()
-
-     function changeCurrentSlideT() {
-       const slideT = document.querySelector('.t')
-       if (slideT) {
-         var children = slideT.children
-         for (var i = 0; i < children.length; i++) {
-           children[i].classList.remove('active')
-         }
-         children[this.currentSlide].classList.add('active')
-       }
-     }
-
-     let childrenT = this.$refs.controlsT.children
-     for (let i = 0; i < childrenT.length; i++) {
-       childrenT[i].classList.remove('active')
-     }
-     childrenT[0].classList.add('active')
-
-     let root = this
-
-     mentorsSiema = new Siema({
-        selector: '.mentors-carousel',
-        duration: 1000,
-        easing: 'ease-out',
-        perPage: {
-          1150: 4,
-          900: 3,
-          650: 2,
-          520: 3,
-          0: 2
-        },
-        startIndex: 0,
-        draggable: true,
-        multipleDrag: true,
-        threshold: 20,
-        loop: false,
-        rtl: false,
-        onInit: changeCurrentSlideM,
-        onChange: changeCurrentSlideM
-     })
-
-     this.playMSiema()
-
-     function changeCurrentSlideM() {
-       var index = this.currentSlide / this.perPage
-       if (this.currentSlide % this.perPage === 0 || index + 1 === root.mentors.set.length / this.perPage) {
-         const slideM = document.querySelector('.m')
-         if (slideM) {
-           var children = slideM.children
-           for (var i = 0; i < children.length; i++) {
-             children[i].classList.remove('active')
-           }
-           index = Math.ceil(index)
-           children[index].classList.add('active')
-         }
-       }
-     }
-
-     addEventListener('resize', function() {
-       root.perPage = mentorsSiema.perPage
-       const slideM = document.querySelector('.m')
-       if (slideM) {
-         var children = slideM.children
-         for (var i = 0; i < children.length; i++) {
-           children[i].classList.remove('active')
-         }
-         children[0].classList.add('active')
-         mentorsSiema.currentSlide = 0
-       }
-     }, false)
-
-     let childrenM = this.$refs.controlsT.children
-     for (let i = 0; i < childrenM.length; i++) {
-       childrenM[i].classList.remove('active')
-     }
-     childrenM[0].classList.add('active')
-
-     this.perPage = mentorsSiema.perPage
-   }
+    this.perPage = mentorsSiema.perPage
+  }
 }
--- spa/src/store/app.store.js
+++ spa/src/store/app.store.js
@@ -4,35 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-   state: {
-      icon: '',
-      lang: '',
-      logo: '',
-      direction: '',
-      forumLogo: '',
-      isLogin: false,
-      githubFileURL: '',
-      progress: {
-         size: 0,
-         width: 0,
-         pageText: '',
-         lessonText: ''
-      },
-      css: {
-         workshops: {
-            drawerWidth: ''
-         }
+  state: {
+    i18n: '',
+    icon: '',
+    lang: '',
+    logo: '',
+    direction: '',
+    forumLogo: '',
+    isLogin: false,
+    githubFileURL: '',
+    progress: {
+      size: 0,
+      width: 0,
+      pageText: '',
+      lessonText: ''
+    },
+    css: {
+      workshops: {
+        drawerWidth: ''
       }
-   },
-   mutations: {
-      getGithubFileURL(state, params) {
-         params.owner = params.owner === undefined ? 'coretabs-academy' : params.owner
-         state.githubFileURL = `https://raw.githubusercontent.com/${params.owner}/${params.repo}/master/${params.path}`
-      }
-   },
-   actions: {
-      getImgUrl(state, img) {
-         return require(`@/assets/multimedia/${img}`)
-      }
-   }
+    }
+  },
+  mutations: {
+    getGithubFileURL(state, params) {
+      params.owner = params.owner === undefined ? 'coretabs-academy' : params.owner
+      state.githubFileURL = `https://raw.githubusercontent.com/${params.owner}/${params.repo}/master/${params.path}`
+    }
+  },
+  actions: {
+    getImgUrl(state, img) {
+      return require(`@/assets/multimedia/${img}`)
+    }
+  }
 })
