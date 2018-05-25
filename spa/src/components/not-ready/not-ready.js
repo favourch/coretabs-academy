3fea76fc4e2aba19147716dde6f16c840e110bf9
Alhakeem <alhakeem.prof@gmail.com>
2018-05-25 22:50:34 +0300

    Update I'm not ready page images

--- spa/src/components/not-ready/not-ready.js
+++ spa/src/components/not-ready/not-ready.js
@@ -13,25 +13,21 @@ export default {
     source_text: 'الإحصائيات التالية من إستطلاع قام به أكثر من 100,000 مبرمج من أنحاء العالم في العام 2018 إستناداً لموقع <a href="https://insights.stackoverflow.com/survey/2018/" target="_blank">Stackoverflow</a>',
     statistics: [
       {
-        imgUrl: '~@/assets/multimedia/images/not-ready/self-taught.svg',
         desc: 'تعلموا البرمجة تعليم ذاتي، بعيداً عن التعليم الجامعي',
         value: '90%',
         reverse: false
       },
       {
-        imgUrl: '~@/assets/multimedia/images/not-ready/self-taught.svg',
         desc: 'حاصلين على وظيفة أو يعملون بدوام جزئي إما في شركات أو كعمل حر',
         value: '80%',
         reverse: true
       },
       {
-        imgUrl: '~@/assets/multimedia/images/not-ready/self-taught.svg',
         desc: 'متوسط الدخل الشهري للفرد أي بمعدل 56000 دولار سنوياً',
         value: '$4666',
         reverse: false
       },
       {
-        imgUrl: '~@/assets/multimedia/images/not-ready/self-taught.svg',
         desc: 'يعملون في قطاعات أخرى غير قطاع تقنية المعلومات ',
         value: '46%',
         reverse: true
--- spa/src/components/not-ready/not-ready.vue
+++ spa/src/components/not-ready/not-ready.vue
@@ -1,14 +1,13 @@
 <template>
     <v-container id="not-ready" fluid >
       <v-layout align-center justify-center>
-        <v-flex id="container" class="text-xs-center" align-center xs12 sm10 lg6>
+        <v-flex id="container" class="text-xs-center" align-center xs12 sm10 lg7>
          <div id="statistics-padding">
             <h1 class="black--text mb-4 mt-5" v-html="heading_text[0]"></h1>
             <p class="black--text mb-5" v-html="description_text[0]"></p>
             <p class="black--text mb-5" id="source-text" v-html="source_text"></p>
             <v-layout v-for="(statistic,index) in statistics" :reverse="statistic.reverse" :key="index" row wrap align-center justify-center mb-5 pt-5>
-              <v-flex :class="['statistics-img','text-sm-center', statistic.reverse ? 'text-md-right': 'text-md-left']" sm12 md6 >
-                <img src="~@/assets/multimedia/images/not-ready/self-taught.svg" alt="">
+              <v-flex :class="['statistics-img', 'img-'+ index]" sm12 md6>
               </v-flex>
               <v-flex :class="['statistics-desc','text-sm-center', statistic.reverse ? 'text-md-left': 'text-md-right']" sm12 md6 >
                 <span v-html="statistic.value"></span>
@@ -17,10 +16,10 @@
             </v-layout>
           </div>
           <v-layout >
-            <div id="test">
+            <div id="go-signup">
               <h1 class="black--text mb-4 mt-5" v-html="heading_text[1]"></h1>
               <p class="black--text mb-5" v-html="description_text[1]"></p>
-              <v-btn id="yes" round dark v-html="button_text"></v-btn>
+              <v-btn id="yes-button" to="/signup" round dark v-html="button_text"></v-btn>
             </div>
           </v-layout>
         </v-flex>
