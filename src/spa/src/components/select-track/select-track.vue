aae9988c003056249476c1845aa8043e00bda8de
Louay Hamada <loauy.hamada@gmail.com>
2018-06-13 05:07:53 +0300

    Enhance Select Track & Lesson Components Styles

--- src/spa/src/components/select-track/select-track.vue
+++ src/spa/src/components/select-track/select-track.vue
@@ -4,24 +4,24 @@
       <v-flex id="container" xs9 md6>
         <v-layout row align-center justify-center wrap>
           <v-flex class="track" xs12 sm6 md6>
-            <div @click="select('1')">
+            <div @click="select(1)">
               <input type="radio" name="track" v-model="track_selected" value="1" />
-              <span class="custom-radio"></span>
-              <div class="icon"></div>
+              <v-icon class="custom-radio"></v-icon>
+              <div class="logo"></div>
               <p v-html="i18n.track_1_text_en" class="mt-4 mb-2"></p>
               <p v-html="i18n.track_1_text_ar" class="mt-2 mb-4"></p>
             </div>
-            <v-btn round class="submit" @click="submit('1')" v-html="i18n.submit_btn_text" :disabled="track_selected != 1"></v-btn>
+            <v-btn round class="submit" @click="submit()" v-html="i18n.submit_btn_text" :disabled="track_selected != 1"></v-btn>
           </v-flex>
           <v-flex class="track" xs12 sm6 md6>
-            <div @click="select('2')">
+            <div @click="select(2)">
               <input type="radio" name="track" v-model="track_selected" value="2" />
-              <span class="custom-radio"></span>
-              <div class="icon"></div>
+              <v-icon class="custom-radio"></v-icon>
+              <div class="logo"></div>
               <p v-html="i18n.track_2_text_en" class="mt-4 mb-2"></p>
               <p v-html="i18n.track_2_text_ar" class="mt-2 mb-4"></p>
             </div>
-            <v-btn round class="submit" @click="submit('2')" v-html="i18n.submit_btn_text" :disabled="track_selected != 2"></v-btn>
+            <v-btn round class="submit" @click="submit()" v-html="i18n.submit_btn_text" :disabled="track_selected != 2"></v-btn>
           </v-flex>
         </v-layout>
       </v-flex>
