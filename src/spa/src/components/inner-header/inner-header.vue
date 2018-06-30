ba1813387ea747f6da1b1b8f7cb1fcca8685f8bc
KHALED_LAKEHAL <khaledcollo2011@gmail.com>
2018-06-30 23:26:57 +0100

    logo from inner-header

--- src/spa/src/components/inner-header/inner-header.vue
+++ src/spa/src/components/inner-header/inner-header.vue
@@ -4,6 +4,9 @@
     <v-toolbar-side-icon v-on:click="$parent.$emit('toggle-drawer')"></v-toolbar-side-icon>
     <v-toolbar-title class="mx-auto">{{title}}</v-toolbar-title>
     <nav>
+      <router-link to="/">
+        <img :src="$store.state.icon" alt="coretabs" />
+      </router-link>
       <v-menu :close-on-content-click="false" v-model="menu" content-class="notifications-menu" offset-y>
         <v-btn slot="activator" class="menu" :class="{'unread': unread}" @click="unread = false">
           <img :src="$store.getters.user('avatar_url')" />
