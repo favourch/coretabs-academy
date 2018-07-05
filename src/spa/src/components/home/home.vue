91051b8310b4e63ddcd2b8de03692ba7e961a482
Alhakeem <alhakeem.prof@gmail.com>
2018-07-05 16:03:43 +0300

    Update Home page take action buttons

--- src/spa/src/components/home/home.vue
+++ src/spa/src/components/home/home.vue
@@ -14,8 +14,8 @@
             <p class="black--text mt-4 mb-0" v-html="i18n.description_text_1"></p>
             <p class="black--text mt-4 mb-0" v-html="i18n.description_text_2"></p>
             <div id="introduction-buttons">
-              <v-btn id="yes" round dark class="mt-4 mb-0 mr-0 ml-3" v-html="i18n.yes_btn_text"></v-btn>
-              <v-btn id="no" round light class="mt-4 mb-0 mx-0" v-html="i18n.no_btn_text"></v-btn>
+              <v-btn id="yes" round dark class="mt-4 mb-0 mr-0 ml-3" v-html="i18n.yes_btn_text"  to="/signup"></v-btn>
+              <v-btn id="no" round light class="mt-4 mb-0 mx-0" v-html="i18n.no_btn_text" to="/not-ready"></v-btn>
             </div>
         </v-flex>
       </v-layout>
