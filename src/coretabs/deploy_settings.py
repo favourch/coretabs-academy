277113549fe6af6497a16e2dc1322337ad4eddbf
0xRumple <kasseryasser@windowslive.com>
2018-07-07 18:53:57 +0300

    Add SPA_BASE_URL env var

--- src/coretabs/deploy_settings.py
+++ src/coretabs/deploy_settings.py
@@ -2,18 +2,14 @@ from .settings import *
 import dj_database_url
 
 
+SPA_BASE_URL = os.environ.get(SPA_BASE_URL)
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '.herokuapp.com',
-    '.compute.amazonaws.com',
     'www.coretabs.net',
     'coretabs.net',
-    '192.168.99.100',
-    '172.17.0.1',
-    '0.0.0.0'
 ]
 
 DATABASES = {
