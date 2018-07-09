0f9d54a9d0907a9ebc264ba05acfd1a38c363849
Alhakeem <alhakeem.prof@gmail.com>
2018-07-09 18:57:32 +0300

    Add 404 error page

--- src/spa/src/components/not-found/not-found.js
+++ src/spa/src/components/not-found/not-found.js
@@ -1,3 +1,22 @@
-export default {
-   name: 'NotFoundComponent'
-}
+export default { 
+  name: 'NotFoundComponent', 
+  methods: { 
+   setSplashHeight() { 
+     var sDiv = document.querySelector('#splash') 
+     if (sDiv) { 
+       var sDivHeight = sDiv.clientWidth - 20 
+       sDiv.setAttribute('style', 'height: ' + sDivHeight + 'px !important') 
+     } 
+   } 
+  }, 
+  mounted() { 
+     window.addEventListener('resize', this.setSplashHeight) 
+     this.$nextTick(function() { this.setSplashHeight() }) 
+   }, 
+   beforeDestroy() { 
+     window.removeEventListener('resize', this.onResize) 
+  }, 
+  computed: { 
+   i18n() { return this.$store.state.i18n.not_found } 
+ } 
+} 
