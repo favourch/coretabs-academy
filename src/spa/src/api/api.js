2a44f9cd6f634cd83858c27c04f569cd4d62fe5b
Louay Hamada <loauy.hamada@gmail.com>
2018-07-16 00:15:46 +0300

    Remove Encryption Functions from Prototypes

--- src/spa/src/api/api.js
+++ src/spa/src/api/api.js
@@ -3,7 +3,6 @@ import AuthAPI from './auth'
 import DateAPI from './date'
 import MarkdownAPI from './markdown'
 import WorkshopsAPI from './workshops'
-import EncryptionAPI from './encryption'
 
 export default {
    install: (Vue) => {
@@ -11,7 +10,6 @@ export default {
       Vue.prototype.$auth = AuthAPI
       Vue.prototype.$api = WorkshopsAPI
       Vue.prototype.$markdown = MarkdownAPI
-      Vue.prototype.$encryption = EncryptionAPI
       Vue.prototype.$util = {
          prettyDigit(n) {
             return (n < 10) ? `0${n}` : n
