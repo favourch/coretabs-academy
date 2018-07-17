92ca0cbfb18c0cd0de886efe669c547a1f3185ee
Louay Hamada <loauy.hamada@gmail.com>
2018-07-17 19:30:16 +0300

    Use Compressed Versions of JS & CSS Files

--- src/spa/server.js
+++ src/spa/server.js
@@ -4,6 +4,21 @@ const app = express()
 const fs = require('fs')
 const path = require('path')
 
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz'
+  console.log(req.url)
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/javascript')
+  next()
+})
+
+app.get('*.css', (req, res, next) => {
+  req.url = req.url + '.gz'
+  res.set('Content-Encoding', 'gzip')
+  res.set('Content-Type', 'text/css')
+  next()
+})
+
 const staticFileMiddleware = express.static('public_html')
 app.use(staticFileMiddleware)
 app.use(history({
@@ -19,6 +34,7 @@ app.get('*', (req, res) => {
    res.write(indexHTML)
    res.end()
 })
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
    console.log(`server started as http://localhost:${port}`)
--- src/spa/vue.config.js
+++ src/spa/vue.config.js
@@ -1,33 +1,44 @@
+const CompressionPlugin = require('compression-webpack-plugin')
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const path = require('path')
-module.exports = {
-   outputDir: 'public_html',
-   configureWebpack: {
-      output: {
-         publicPath: '/',
-         path: path.resolve(__dirname, './public_html')
-      },
-      plugins: [
-         new FaviconsWebpackPlugin({
-            logo: './src/assets/multimedia/icons/icon.png',
-            prefix: 'icons/',
-            persistentCache: true,
-            inject: true,
-            background: '#fff',
-            title: 'Coretabs Academy',
-            icons: {
-               android: true,
-               appleIcon: true,
-               appleStartup: true,
-               coast: false,
-               favicons: true,
-               firefox: true,
-               opengraph: true,
-               twitter: true,
-               yandex: true,
-               windows: true
-            }
-         })
-      ]
-   }
+var config = {
+  outputDir: 'public_html',
+  configureWebpack: {
+    output: {
+      publicPath: '/',
+      path: path.resolve(__dirname, './public_html')
+    },
+    plugins: [
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: true
+      }),
+      new FaviconsWebpackPlugin({
+        logo: './src/assets/multimedia/icons/icon.png',
+        prefix: 'icons/',
+        persistentCache: true,
+        inject: true,
+        background: '#fff',
+        title: 'Coretabs Academy',
+        icons: {
+            android: true,
+            appleIcon: true,
+            appleStartup: true,
+            coast: false,
+            favicons: true,
+            firefox: true,
+            opengraph: true,
+            twitter: true,
+            yandex: true,
+            windows: true
+        }
+      })
+    ]
+  }
 }
+
+module.exports = config
