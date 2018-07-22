513669153ba6183e6bb8fdfb68d7f885d5cdaa7a
Hichem2h <hichem2h@gmail.com>
2018-07-22 19:03:04 +0100

    edit: admin site always english

--- src/coretabs/settings/base.py
+++ src/coretabs/settings/base.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django_logging.middleware.DjangoLoggingMiddleware',
+    "coretabs.middleware.AdminLocaleMiddleware",
 ]
 
 ROOT_URLCONF = 'coretabs.urls'
