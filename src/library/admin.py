f6cca9bc0e2d3ac473d0a0cd5d991279d3b0712b
Hichem2h <hichem2h@gmail.com>
2018-07-23 23:32:39 +0100

    add: profile search by username or fullname

--- src/library/admin.py
+++ src/library/admin.py
@@ -34,6 +34,9 @@ class TrackWorkshopInline(SortableInlineAdminMixin, admin.TabularInline):
 class TrackAdmin(admin.ModelAdmin):
     inlines = (TrackWorkshopInline,)
 
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ('user__username', 'user__first_name',)
+
 
 site.register(models.MarkdownLesson)
 site.register(models.QuizLesson)
@@ -43,4 +46,4 @@ site.register(models.WorkshopModule)
 site.register(models.Workshop, WorkshopAdmin)
 site.register(models.TrackWorkshop)
 site.register(models.Track, TrackAdmin)
-site.register(models.Profile)
+site.register(models.Profile, ProfileAdmin)
