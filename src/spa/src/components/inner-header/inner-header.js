446f8e4659655ecdb8f2bc80b628a2041fc15527
Louay Hamada <loauy.hamada@gmail.com>
2018-07-27 20:11:07 +0300

    Mark Unread Notification as Read on Click

--- src/spa/src/components/inner-header/inner-header.js
+++ src/spa/src/components/inner-header/inner-header.js
@@ -33,6 +33,11 @@ export default {
         default:
           return 'notifications'
       }
+    },
+    mark_read(notification) {
+      if (!notification.read) {
+        notification.read = true
+      }
     }
   },
   created() {
