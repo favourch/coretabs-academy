95ca80158c82b9d3e675a3425dc7e987f41410d3
Louay Hamada <loauy.hamada@gmail.com>
2018-08-02 01:46:02 +0300

    Set Server Config

---
+++ src/spa/server.config.js
@@ -0,0 +1,48 @@
+var path = require('path')
+var merge = require('webpack-merge')
+var VueConfig = require('./client.config')
+
+delete VueConfig.module
+
+var ServerConfig = merge(VueConfig, {
+  target: 'node',
+  entry: {
+    app: './src/server-entry.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './static'),
+    filename: 'js/server.app.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.(ttf|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  externals: Object.keys(require('./package.json').dependencies)
+})
+
+ServerConfig.plugins = [ServerConfig.plugins.shift()]
+module.exports = ServerConfig
