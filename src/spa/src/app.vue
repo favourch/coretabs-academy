780a7c98a4ffdfd1f019c2c53afa82231167623b
Louay Hamada <loauy.hamada@gmail.com>
2018-08-01 05:16:45 +0300

    Remove "drawerIcon" setProperty Functions

--- src/spa/src/app.vue
+++ src/spa/src/app.vue
@@ -1,12 +1,12 @@
 <template>
-<v-app>
-   <header-component></header-component>
-   <v-content>
+  <v-app>
+    <header-component></header-component>
+    <v-content>
       <v-container fluid class="main-container">
-         <router-view></router-view>
+        <router-view></router-view>
       </v-container>
-   </v-content>
-</v-app>
+    </v-content>
+  </v-app>
 </template>
 <script src="./app.js"></script>
 <style src="./app.scss" lang="scss"></style>
