8fff24dcdcae3b7b4e79216689841503b88345ae
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-08-09 03:18:35 +0200

    fix: exporting env vars in remote server

--- gitlab-deploy/.gitlab-deploy.prod.sh
+++ gitlab-deploy/.gitlab-deploy.prod.sh
@@ -11,5 +11,32 @@ array=(${string//,/ })
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
 for i in "${!array[@]}"; do
   echo "Deploy project on server ${array[i]}"
-  ssh ubuntu@${array[i]} "cd /var/academy && git stash  && sudo git checkout $CI_BUILD_REF_NAME && git stash && sudo git pull origin master && docker-compose -f docker-compose.production.yml up --force-recreate --build -d"
+  ssh ubuntu@${array[i]} <<EOF
+  export HOST_ENV=$HOST_ENV /
+            API_BASE_URL=$API_BASE_URL /
+            SPA_BASE_URL=$SPA_BASE_URL /
+            DATABASE_URL=$DATABASE_URL /
+            DEFAULT_FROM_EMAIL=$DEFAULT_FROM_EMAIL /
+            DISCOURSE_API_KEY=$DISCOURSE_API_KEY /
+            DISCOURSE_API_USERNAME=$DISCOURSE_API_USERNAME /
+            DISCOURSE_BASE_URL=$DISCOURSE_BASE_URL /
+            DISCOURSE_SSO_SECRET=$DISCOURSE_SSO_SECRET /
+            EMAIL_HOST=$EMAIL_HOST /
+            EMAIL_HOST_PASSWORD=$EMAIL_HOST_PASSWORD /
+            EMAIL_HOST_USER=$EMAIL_HOST_USER /
+            MANAGERS_EMAILS=$MANAGERS_EMAILS /
+            SECRET_KEY=$SECRET_KEY /
+            SENTRY_DSN=$SENTRY_DSN /
+            POSTGRES_DB=$POSTGRES_DB /
+            POSTGRES_USER: $POSTGRES_USER /
+            POSTGRES_PASSWORD: $POSTGRES_PASSWORD /
+            MAINTENANCE_MODE='0' /
+
+cd /var/academy
+git stash 
+sudo git checkout $CI_BUILD_REF_NAME
+git stash
+sudo git pull origin master 
+docker-compose -f docker-compose.production.yml up --force-recreate --build -d
+EOF
 done
