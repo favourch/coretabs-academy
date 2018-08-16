9b1b7985d8342e4de9f5b95719267c9f42976397
Louay Hamada <loauy.hamada@gmail.com>
2018-08-16 14:12:19 +0300

    Use SSR Bundle Renderer

--- src/spa/express.js
+++ src/spa/express.js
@@ -4,8 +4,13 @@ const fs = require('fs')
 const path = require('path')
 
 const template = fs.readFileSync(path.resolve(__dirname, './static/index.html'), 'utf-8')
-const bundle = require('./static/js/server.app.js')
-const renderer = require('vue-server-renderer').createRenderer(bundle)
+
+const bundle = require('./static/vue-ssr-server-bundle.json')
+const bundleRenderer = require('vue-server-renderer').createBundleRenderer(bundle, {
+  runInNewContext: false,
+  template,
+  inject: false
+})
 
 server.get('*.js', (req, res, next) => {
   req.url = req.url + '.gz'
@@ -24,28 +29,23 @@ server.get('*.css', (req, res, next) => {
 server.use('/static', express.static(path.join(__dirname, './static'), { maxAge: '356d' }))
 
 server.get('*', (req, res) => {
-  bundle.default({url: req.url}).then((app) => {
-    renderer.renderToString(app, (err, html) => {
-      if (err) {
-        if (err.code === 404) {
-          res.status(404).end('Page not found')
-        } else {
-          console.log('err', err)
-          res.status(500).end('Internal Server Error')
-        }
+  const context = { url: req.url }
+  bundleRenderer.renderToString(context, (err, html) => {
+    if (err) {
+      if (err.code === 404) {
+        res.status(404).end('Page not found')
       } else {
-        html = template.replace('{{ APP }}', html)
-        res.setHeader('Content-Type', 'text/html')
-        res.write(html)
-        res.send()
+        console.log('err', err)
+        res.status(500).end('Internal Server Error')
       }
-    })
-  }, (err) => {
-    console.log(err)
+    } else {
+      res.setHeader('Content-Type', 'text/html')
+      res.end(html)
+    }
   })
 })
 
 const port = process.env.PORT || 3000
 server.listen(port, () => {
-   console.log(`server started as http://localhost:${port}`)
+  console.log(`server started as http://localhost:${port}`)
 })
