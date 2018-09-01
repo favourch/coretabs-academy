f2f47ad4a2eda59e57a13bf7dca3530b38e79ae2
Alhakeem <alhakeem.prof@gmail.com>
2018-09-01 23:07:08 +0300

    Update video lessons iframe

--- src/spa/src/components/lesson/lesson.vue
+++ src/spa/src/components/lesson/lesson.vue
@@ -3,7 +3,7 @@
     <template v-if="['0', '1'].includes($parent.current.lesson.type)">
       <div :id="($parent.current.lesson.type === '0') ? 'lesson-youtube' : 'lesson-scrimba'"
         :class="['lesson-video', ($parent.current.lesson.type === '0') ? 'lesson-youtube' : 'lesson-scrimba']">
-        <iframe v-if="$parent.current.lesson.type === '0'" :src="content.video+'?showinfo=0&rel=0'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe v-if="$parent.current.lesson.type === '0'" :src="content.video+'?showinfo=0&rel=0'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen id="youtube-iframe"></iframe>
         <iframe v-else :src="content.video"></iframe>
         <v-tabs :id="($parent.current.lesson.type === '0') ? 'youtube-tabs' : 'scrimba-tabs'" :right="$store.state.direction === 'rtl'" icons-and-text v-model="content.tab">
           <v-tab v-for="(tab, i) in i18n.video.tabs" :key="i">
