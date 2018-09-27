1fac8a484e162591a9463921ab3b2225e8cbd984
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-09-27 03:29:40 +0200

    Add static media settings

--- src/coretabs/settings/prod.py
+++ src/coretabs/settings/prod.py
@@ -111,3 +111,16 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 AWS_QUERYSTRING_AUTH = False # This will make sure that the file URL does not have unnecessary parameters like your access key.
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+#static media settings
+
+STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = STATIC_URL + 'media/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = 'static_root'
+ADMIN_MEDIA_PREFIX = f'{STATIC_URL}admin/'
+
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
