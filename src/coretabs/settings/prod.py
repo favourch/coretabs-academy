edece1bf55740a97500490a319e8f14d05e80d33
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-09-30 06:23:15 +0200

    Add media_root to prod.py settings

--- src/coretabs/settings/prod.py
+++ src/coretabs/settings/prod.py
@@ -111,6 +111,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 MEDIA_URL = STATIC_URL + 'media/'
+MEDIA_ROOT = MEDIA_URL
 STATICFILES_DIRS = ( 
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
