fdfa8ace18838d13a8777b56f1e652cdcfda95ae
0xRumple <kasseryasser@windowslive.com>
2018-10-08 18:52:36 +0300

    Add dev secrets file

--- src/api/coretabs/settings/__init__.py
+++ src/api/coretabs/settings/__init__.py
@@ -5,3 +5,9 @@ if os.environ.get('HOST_ENV') == 'production':
     from .prod import *
 else:
     from .dev import *
+
+if True:
+    try:
+        from .dev_secrets import *
+    except ModuleNotFoundError:
+        pass
