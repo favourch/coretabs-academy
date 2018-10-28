a47b887ecabfc274f887f169494c1057eba40673
Louay Hamada <loauy.hamada@gmail.com>
2018-10-28 05:58:24 +0300

    Fix Confirmation Email Sending Issue

--- src/spa/src/components/congratulations/congratulations.js
+++ src/spa/src/components/congratulations/congratulations.js
@@ -35,6 +35,9 @@ export default {
   },
   mounted() {
     if (!this.email) { this.$router.push('/') }
+    let root = this
+    root.waiting = true
+    root.waiting = await this.$auth.confirmation(root)
     setInterval(() => {
       if (this.counter > 0) {
         this.counter -= 1
