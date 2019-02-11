1ae29466efcd94c942bbf8a0452ab36fbcef3611
hichem2h <hichem2h@gmail.com>
2019-02-11 18:04:04 +0100

    add: generate certificates admin action

--- src/api/profiles/admin.py
+++ src/api/profiles/admin.py
@@ -1,6 +1,11 @@
-from coretabs.admin import site
+from django.conf import settings
+
+from coretabs.admin import site, MyActionForm
+
 from django.contrib.admin import ModelAdmin, TabularInline
 from .models import Profile, SocialLink, Team, Certificate, CertificateTemplate, CertificateSignature
+
+from .utils import render_mail
 
 
 class SocialLinkInline(TabularInline):
@@ -9,7 +14,28 @@ class SocialLinkInline(TabularInline):
 
 
 class ProfileAdmin(ModelAdmin):
+    action_form = MyActionForm
     inlines = (SocialLinkInline,)
+    actions = ['generate_and_send_certificates']
+
+    def _send_certificate_email(self, certificate, email):
+        context = {'certificate_url': f'{settings.SPA_BASE_URL}/certificates/{certificate.id}', }
+
+        msg = render_mail(
+            'profiles/email/certificate',
+            email,
+            context)
+        msg.send()
+
+    def generate_and_send_certificates(self, request, queryset):
+        template_heading = request.POST['x']
+        template = CertificateTemplate.objects.get(heading=template_heading)
+
+        for profile in queryset:
+            # TODO: see if profile has already this certificate
+            certificate = Certificate(profile=profile, full_name=profile.user.first_name, template=template)
+            certificate.save()
+            self._send_certificate_email(certificate, profile.user.email)
 
 
 site.register(Profile, ProfileAdmin)
