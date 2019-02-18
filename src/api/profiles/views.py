ec46e58169a86496af4cecebaf8066b6bed4e650
hichem2h <hichem2h@gmail.com>
2019-02-18 22:56:31 +0100

    add: my profile view and url

--- src/api/profiles/views.py
+++ src/api/profiles/views.py
@@ -1,10 +1,19 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateAPIView, RetrieveAPIView
 from django.shortcuts import get_object_or_404
 from .models import Profile, Certificate
 from .serializers import ProfileSerializer, CertificateSerializer
 
 
-class ProfileView(RetrieveUpdateAPIView):
+class MyProfileView(RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user.profile
+
+
+class ProfileView(RetrieveAPIView):
     serializer_class = ProfileSerializer
 
     def get_object(self):
