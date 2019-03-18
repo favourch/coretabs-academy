ca9f83952ef560e5604cf5a25ce3e199f85faa71
Alhakeem <alhakeem.prof@gmail.com>
2019-03-18 17:18:42 +0300

    Add Certificate share button

--- src/spa/src/components/certificate/certificate.js
+++ src/spa/src/components/certificate/certificate.js
@@ -1,6 +1,38 @@
 export default {
   name: 'certificate',
   data: () => ({
-    
-  })
+    shareDialog: false,
+    certificateLink: '',
+    isCopied: false,
+    manualCopy: false
+  }),
+  methods: {
+    shareCertificate() {
+      this.shareDialog = true
+      this.certificateLink = window.location.href
+    },
+    closeDialog() {
+      this.shareDialog = false
+    },
+    copyToClipboard() {
+      let link = document.querySelector('#link-holder')
+      link.setAttribute('type', 'text') 
+      link.select()
+      try {
+        document.execCommand('copy')
+        this.isCopied = true
+        setTimeout(() => {
+          this.isCopied = false
+        }, 2000)
+      } catch (err) {
+        this.isCopied = true
+        this.manualCopy = true
+        setTimeout(() => {
+          this.isCopied = false
+        }, 2000)
+      }
+      link.setAttribute('type', 'hidden')
+      window.getSelection().removeAllRanges()
+    }
+  }
 }
--- src/spa/src/components/certificate/certificate.vue
+++ src/spa/src/components/certificate/certificate.vue
@@ -42,15 +42,38 @@
     <v-flex id="certificate-actions" sm12 md12>
 
       <v-layout row align-center justify-center wrap>
-         <v-flex xs12 md2 px-3>
-           <v-btn class="mb-3" large > <v-icon dark>save_alt</v-icon>تنزيل</v-btn>
-         </v-flex>
-         <v-flex xs12 md2 px-3>
-           <v-btn dark color="green" large > <v-icon dark>share</v-icon> مشاركة</v-btn>
-         </v-flex>
+        <v-flex xs12 md2 px-3>
+          <v-btn class="mb-3" large>
+            <v-icon dark>save_alt</v-icon>تنزيل
+          </v-btn>
+        </v-flex>
+        <v-flex xs12 md2 px-3>
+          <v-btn dark color="green" large @click.stop="shareCertificate">
+            <v-icon dark>share</v-icon> مشاركة
+          </v-btn>
+        </v-flex>
       </v-layout>
 
+      <v-dialog v-model="shareDialog" max-width="500">
+        <v-card class="pa-2">
+          <v-card-text v-if="!isCopied">
+            <p>قم بنسخ الرابط التالي ومشاركتة مع زملائك</p>
+            <div id="certificate-link-holder" @click="copyToClipboard">{{certificateLink}}</div>
+            <input type="hidden" id="link-holder" :value="certificateLink">
+          </v-card-text>
+            <v-card-text class="text-xs-center" v-if="isCopied">
+              <v-icon color="green" class="mb-3" large>check_circle</v-icon>
+              <p class="subheading">تم نسخ الرابط بنجاح، بإمكانك الأن مشاركتة</p>
+            </v-card-text>
+            <v-card-text v-if="manualCopy">
+              <p>قم بنسخ الرابط بشكل يدوي، ثم قم بمشاركته</p>
+              <v-text-field :value="certificateLink"></v-text-field>
+            </v-card-text>
+        </v-card>
+      </v-dialog>
+
     </v-flex>
+
   </v-layout>
 </v-container>
 </template>
