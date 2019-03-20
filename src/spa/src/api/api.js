e5503bae4794308d453db004f7b0173c46cef4a3
Unknown <ayman97rf@gmail.com>
2019-03-20 19:16:27 +0200

    Add profiles api calls
    Added profiles calls and certificates in the same file.

--- src/spa/src/api/api.js
+++ src/spa/src/api/api.js
@@ -3,6 +3,8 @@ import AuthAPI from './auth'
 import DateAPI from './date'
 import MarkdownAPI from './markdown'
 import WorkshopsAPI from './workshops'
+import ProfilesAPI from './profiles'
+import CertificatesAPI from './profiles'
 
 export default {
    install: (Vue) => {
@@ -10,6 +12,8 @@ export default {
       Vue.prototype.$auth = AuthAPI
       Vue.prototype.$api = WorkshopsAPI
       Vue.prototype.$markdown = MarkdownAPI
+      Vue.prototype.$profiles = ProfilesAPI
+      Vue.prototype.$certificates = CertificatesAPI
       Vue.prototype.$util = {
          prettyDigit(n) {
             return (n < 10) ? `0${n}` : n
