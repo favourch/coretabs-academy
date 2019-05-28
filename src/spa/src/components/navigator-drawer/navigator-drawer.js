2c6ad250a185a4f426f3d79a8c374914f727ad51
Louay Hamada <loauy.hamada@gmail.com>
2019-05-28 22:21:47 +0300

    Show Community Icon in Nav Drawer for Visitors

--- src/spa/src/components/navigator-drawer/navigator-drawer.js
+++ src/spa/src/components/navigator-drawer/navigator-drawer.js
@@ -30,11 +30,15 @@ export default {
       throw new Error(error.message)
     })
 
-    this.items.push({ route: `/classroom/${this.$store.getters.account('track')}/`, icon: 'school', title: this.i18n.classroom })
-    this.items.push({ link: 'https://forums.coretabs.net', icon: 'forum', title: this.i18n.forum })
-    this.items.push({ route: `/user/${this.$store.getters.user('username')}/`, icon: 'person', title: this.i18n.profile, separator: true })
-    this.items.push({ route: '/profile', icon: 'settings', title: this.i18n.settings })
-    this.items.push({ route: '/logout', icon: 'exit_to_app', title: this.i18n.logout })
+    if(this.$store.getters.isLogin) {
+      this.items.push({ route: `/classroom/${this.$store.getters.account('track')}/`, icon: 'school', title: this.i18n.classroom })
+      this.items.push({ link: 'https://forums.coretabs.net', icon: 'forum', title: this.i18n.forum })
+      this.items.push({ route: `/user/${this.$store.getters.user('username')}/`, icon: 'person', title: this.i18n.profile, separator: true })
+      this.items.push({ route: '/profile', icon: 'settings', title: this.i18n.settings })
+      this.items.push({ route: '/logout', icon: 'exit_to_app', title: this.i18n.logout })
+    } else {
+      this.items.push({ link: 'https://forums.coretabs.net', icon: 'forum', title: this.i18n.forum })
+    }
   },
   methods: {
     toggleDrawer(event) {
