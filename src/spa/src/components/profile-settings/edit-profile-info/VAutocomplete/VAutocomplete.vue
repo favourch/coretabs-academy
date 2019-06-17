7ab09b0ded3faaba20d40a320e6f3bca37ed6e78
Alhakeem <alhakeem.prof@gmail.com>
2019-06-17 17:22:53 +0300

    Add transition to country list

--- src/spa/src/components/profile-settings/edit-profile-info/VAutocomplete/VAutocomplete.vue
+++ src/spa/src/components/profile-settings/edit-profile-info/VAutocomplete/VAutocomplete.vue
@@ -3,10 +3,12 @@
   <div tabindex="0" @focusin="hasFocus=true" class="v-autocomplete-select">
     <v-text-field append-icon="arrow_drop_down" v-if="inputValue && !switchInput" :label="form.country" :value="inputValue.text" @focus="switchInput=true"></v-text-field>
     <v-text-field append-icon="arrow_drop_down" v-else :label="form.country" @focus="hasFocus=true" autocomplete="off" ref="search" v-model="search" @keyup="moveToResults" @keydown="removeOption"></v-text-field>
+    <v-slide-y-transition>
     <div v-if="showResultList" ref="resultList" class="countries-list">
       <div tabindex="0" ref="result" class="result" v-for="result in results" :key="result[optionValue]" v-html="highlight(result[optionText])" @click="selectOption(result)" @keyup.prevent="navigateResults(result, $event)">
       </div>
     </div>
+    </v-slide-y-transition>
   </div>
 </div>
 </template>
@@ -21,7 +23,7 @@ export default {
     },
     options: {
       type: Array,
-      default: function () {
+      default: function() {
         return []
       },
       required: true
@@ -43,119 +45,119 @@ export default {
     },
     value: {
       type: Object,
-      default: function () {
+      default: function() {
         return null
       },
       required: false
     }
   },
-  data: function () {
+  data: function() {
     return {
       hasFocus: false,
       search: null,
       selectedOption: this.value,
       selectedResult: 0,
       switchInput: false
-    };
+    }
   },
   mounted() {
-    window.addEventListener("click", this.loseFocus)
+    window.addEventListener('click', this.loseFocus)
   },
   destroyed() {
-    window.removeEventListener("click", this.loseFocus)
+    window.removeEventListener('click', this.loseFocus)
   },
   computed: {
-    results: function () {
-      return this.search ? this.options.filter(i => String(i[this.optionText]).toLowerCase().indexOf(this.search.toLowerCase()) > -1) : this.options;
+    results: function() {
+      return this.search ? this.options.filter(i => String(i[this.optionText]).toLowerCase().indexOf(this.search.toLowerCase()) > -1) : this.options
     },
-    showResultList: function () {
-      return this.hasFocus && this.results.length > 0;
+    showResultList: function() {
+      return this.hasFocus && this.results.length > 0
     },
-    showPlaceholder: function () {
-      return !this.hasFocus && !this.selectedOption;
+    showPlaceholder: function() {
+      return !this.hasFocus && !this.selectedOption
     },
     form() { return this.$store.state.i18n.form }
   },
   watch: {
-    hasFocus: function (hasFocus) {
-      window.removeEventListener("keydown", this.stopScroll);
+    hasFocus: function(hasFocus) {
+      window.removeEventListener('keydown', this.stopScroll)
       if (hasFocus) {
-        window.addEventListener("keydown", this.stopScroll);
-        this.$refs.search.focus();
+        window.addEventListener('keydown', this.stopScroll)
+        this.$refs.search.focus()
       } else {
-        this.search = null;
-        this.selectedResult = 0;
-        this.$refs.search.blur();
+        this.search = null
+        this.selectedResult = 0
+        this.$refs.search.blur()
       }
     },
-    value: function () {
+    value: function() {
       this.options.forEach(option => {
         if (this.value && option[this.optionValue] == this.value[this.optionValue]) {
-          this.selectedOption = option;
+          this.selectedOption = option
         }
       })
     },
-    selectedOption: function () {
-      this.$emit('input', this.selectedOption);
+    selectedOption: function() {
+      this.$emit('input', this.selectedOption)
       console.log(this.selectedOption)
       this.search = this.selectedOption.text
     },
-    search: function () {
-      this.$emit('search', this.search);
+    search: function() {
+      this.$emit('search', this.search)
     }
   },
   methods: {
-    selectOption: function (option) {
-      this.selectedOption = option;
-      this.hasFocus = false;
+    selectOption: function(option) {
+      this.selectedOption = option
+      this.hasFocus = false
     },
-    removeOption: function (event) {
-      if (event.keyCode === 8 && (this.search == null || this.search == '')) {
-        this.selectedOption = null;
-        this.hasFocus = false;
+    removeOption: function(event) {
+      if (event.keyCode === 8 && (this.search === null || this.search === '')) {
+        this.selectedOption = null
+        this.hasFocus = false
       }
     },
-    moveToResults: function (event) {
+    moveToResults: function(event) {
       if (event.keyCode === 40) {
         if (this.$refs.result.length > 0) {
-          this.$refs.resultList.children.item(0).focus();
+          this.$refs.resultList.children.item(0).focus()
         }
       }
     },
-    navigateResults: function (option, event) {
+    navigateResults: function(option, event) {
       if (event.keyCode === 13) {
-        this.selectOption(option);
+        this.selectOption(option)
       } else if (event.keyCode === 40 || event.keyCode === 38) {
         if (event.keyCode === 40) {
-          this.selectedResult++;
+          this.selectedResult++
         } else if (event.keyCode === 38) {
-          this.selectedResult--;
+          this.selectedResult--
         }
-        let next = this.$refs.resultList.children.item(this.selectedResult);
+        let next = this.$refs.resultList.children.item(this.selectedResult)
         if (next) {
-          next.focus();
+          next.focus()
         } else {
-          this.selectedResult = 0;
-          this.$refs.search.focus();
+          this.selectedResult = 0
+          this.$refs.search.focus()
         }
       }
     },
-    highlight: function (value) {
+    highlight: function(value) {
       if (this.search) {
-        let matchPos = String(value).toLowerCase().indexOf(this.search.toLowerCase());
+        let matchPos = String(value).toLowerCase().indexOf(this.search.toLowerCase())
         if (matchPos > -1) {
-          let matchStr = String(value).substr(matchPos, this.search.length);
-          value = String(value).replace(matchStr, '<span style="font-weight: bold; background-color: #efefef;">' + matchStr + '</span>');
+          let matchStr = String(value).substr(matchPos, this.search.length)
+          value = String(value).replace(matchStr, '<span style="font-weight: bold; background-color: #efefef;">' + matchStr + '</span>')
         }
       }
-      return value;
+      return value
     },
-    stopScroll: function (event) {
+    stopScroll: function(event) {
       if (event.keyCode === 40 || event.keyCode === 38) {
-        event.preventDefault();
+        event.preventDefault()
       }
     },
-    loseFocus: function (event) {
+    loseFocus: function(event) {
       if (!this.$el.contains(event.target)) {
         this.hasFocus = false
         this.switchInput = false
